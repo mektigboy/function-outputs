b0abf6d25f74e3d868a9cfa73f8cfa153d5b3f63
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-30 13:47:45 -0500

    Update FunctionOutputs.sol

--- FunctionOutputs.sol
+++ FunctionOutputs.sol
@@ -2,18 +2,18 @@
 pragma solidity ^0.8.14;
 
 contract FunctionOutputs {
-  function returnMany() public pure returns (uint, bool) {
-    return (1, true);
-  }
-  function named() public pure returns (uint x, bool b) {
-    return (1, true);
-  }
-  function assigned() public pure returns (uint x, bool b) {
-    x = 1;
-    b = true;
-  }
-  function destructingAssigments() public pure {
-    (uint x, bool b) = returnMany(); // Capture outputs from <returnMany> and assign them a variable name.
-    (, bool _b) = returnMany(); // Capture only the second output from <returnMany>.
-  }
+    function returnMany() public pure returns (uint, bool) {
+        return (1, true);
+    }
+    function named() public pure returns (uint x, bool b) {
+        return (1, true);
+    }
+    function assigned() public pure returns (uint x, bool b) {
+        x = 1;
+        b = true;
+    }
+    function destructingAssigments() public pure {
+        (uint x, bool b) = returnMany(); // Capture outputs from <returnMany> and assign them a variable name.
+        (, bool _b) = returnMany(); // Capture only the second output from <returnMany>.
+    }
 }
